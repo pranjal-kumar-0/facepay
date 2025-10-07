a4c431e993e3810ec3975705d7bc4efadea361fc
Pranjal <kpranjal219@gmail.com>
2025-10-08 05:17:08 +0530

    increased threshold

--- app/api/find_match/route.ts
+++ app/api/find_match/route.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 
-const MATCH_THRESHOLD = 0.92; 
+const MATCH_THRESHOLD = 0.94; 
 
 export async function POST(request: Request) {
   const { face_embedding } = await request.json();
